d8d81eda5fa8f0f26fb4b39e31b33d10299d7a46
Guilherme Mendes <boralagui@outlook.com>
2023-03-14 00:46:07 -0300

    commitando resolução de exercicio

--- SQL/Desafios.sql
+++ SQL/Desafios.sql
@@ -22,3 +22,9 @@ SELECT * FROM city
 
 The CITY table is described as follows:*/
 SELECT * FROM city WHERE ID = '1661'
+
+
+/*Query all attributes of every Japanese city in the CITY table. The COUNTRYCODE for Japan is JPN.
+
+The CITY table is described as follows:*/
+SELECT * FROM city WHERE countrycode = 'JPN'
