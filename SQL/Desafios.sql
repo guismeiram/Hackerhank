a4b7239c35431826eb83888e0b685f5551f3010a
Guilherme Mendes <boralagui@outlook.com>
2023-03-14 00:31:49 -0300

    commitando desafio

--- SQL/Desafios.sql
+++ SQL/Desafios.sql
@@ -2,3 +2,11 @@
 The CITY table is described as follows:*/
 
 SELECT * FROM CITY WHERE CountryCode = 'USA' AND POPULATION > 100000
+
+/*Query the NAME field for all American cities in the CITY table with populations larger than 120000. The CountryCode for America is USA.
+
+The CITY table is described as follows:*/
+
+SELECT name
+FROM city
+WHERE countrycode = 'USA' AND population > 120000
